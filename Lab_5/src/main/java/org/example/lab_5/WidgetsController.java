2426ed1f3c0185f85a90d236461097ba83cc5abf
LevKolechkov <levkolechkov@gmail.com>
2024-10-24 00:49:08 +0700

    feat: Написана форма виджетов

---
+++ Lab_5/src/main/java/org/example/lab_5/WidgetsController.java
@@ -0,0 +1,42 @@
+package org.example.lab_5;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class WidgetsController {
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private CheckBox textFieldCheckBox;
+
+    @FXML
+    private CheckBox buttonCheckBox;
+
+    @FXML
+    private CheckBox labelCheckBox;
+
+    @FXML
+    private void toggleTextFieldVisibility() {
+        textField.setVisible(!textField.isVisible());
+    }
+
+    @FXML
+    private void toggleButtonVisibility() {
+        button.setVisible(!button.isVisible());
+    }
+
+    @FXML
+    private void toggleLabelVisibility() {
+        label.setVisible(!label.isVisible());
+    }
+}
